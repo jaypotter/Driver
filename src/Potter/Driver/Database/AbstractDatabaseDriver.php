941034cafa1cf9afb4dd5a273fd2bc45deaea6d6
Jay Potter <j@ypotter.ca>
2024-09-01 14:29:47 -0400

    remove getResult method

--- src/Potter/Driver/Database/AbstractDatabaseDriver.php
+++ src/Potter/Driver/Database/AbstractDatabaseDriver.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Potter\Driver\Database;
 
-use Potter\Database\{Result\ResultInterface, Statement\StatementInterface};
-use Potter\Driver\DriverInterface;
+use Potter\Database\Statement\StatementInterface;
 
 abstract class AbstractDatabaseDriver implements DatabaseDriverInterface
 {
-    abstract public function getResult(StatementInterface $statement): ResultInterface;
     abstract public function prepare(string $query, object $handle): StatementInterface;
 }
--- src/Potter/Driver/Database/DatabaseDriverInterface.php
+++ src/Potter/Driver/Database/DatabaseDriverInterface.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace Potter\Driver\Database;
 
-use Potter\Database\{Result\ResultInterface, Statement\StatementInterface};
+use Potter\Database\Statement\StatementInterface;
 use Potter\Driver\DriverInterface;
 
 interface DatabaseDriverInterface extends DriverInterface
 {
-    public function getResult(StatementInterface $statement): ResultInterface;
     public function prepare(string $query, object $handle): StatementInterface;
 }
