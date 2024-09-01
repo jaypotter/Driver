b282c301fee8c18042c96988b7673319a6b2bec1
Jay Potter <j@ypotter.ca>
2024-09-01 14:17:18 -0400

    Create AbstractDatabaseDriverAware.php

---
+++ src/Potter/Driver/Database/Aware/AbstractDatabaseDriverAware.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Driver\Database\Aware;
+
+abstract class AbstractDatabaseDriverAware 
+{
+    abstract public function getDatabaseDriver(): DatabaseDriverInterface;
+    abstract public function hasDatabaseDriver(): bool;
+    abstract protected function setDatabaseDriver(?DatabaseDriverInterface $databaseDriver = null): ?DatabaseDriverInterface;
+    abstract protected function unsetDatabaseDriver(): void;
+    abstract public function withDatabaseDriver(?DatabaseDriverInterface $databaseDriver = null): static;
+    abstract public function withoutDatabaseDriver(): static;
+}
